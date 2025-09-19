aa1de5fb03f79be5c6a136f76aaeabffba182007
binh.nguyen <binh.nguyen@sungrove.co.jp>
2025-09-19 10:18:52 +0700

    update vercel

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 import express from "express";
 import Kuroshiro from "kuroshiro";
 import KuromojiAnalyzer from "kuroshiro-analyzer-kuromoji";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const app = express();
 app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const kuroshiro = new Kuroshiro.default();
 const analyzer = new KuromojiAnalyzer({
-  dictPath: path.join(__dirname, "../node_modules/kuromoji/dict"),
+  dictPath: join(__dirname, "./kuromoji_dict"),
 });
 const ready = kuroshiro.init(analyzer);
 
-app.post("/hiragana", async (req, res) => {
+app.post("/", async (req, res) => {
   try {
     await ready;
     const { text, mode } = req.body;
